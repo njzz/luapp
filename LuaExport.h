d5c3ca64908f62d2f1e549ab43d44e33e3b0988d
njzz <55536910+njzz@users.noreply.github.com>
2023-05-09 17:20:59 +0800

    Update LuaExport.h
    update

--- LuaExport.h
+++ LuaExport.h
@@ -378,18 +378,3 @@ namespace app {
 		};
 	}
 }
-
-//待Lua调用的C注册函数。
-//static int add2(lua_State* L)
-//{
-//	//检查栈中的参数是否合法，1表示Lua调用时的第一个参数(从左到右)，依此类推。
-//	//如果Lua代码在调用时传递的参数不为number，该函数将报错并终止程序的执行。
-//	double op1 = luaL_checknumber(L, 1);
-//	double op2 = luaL_checknumber(L, 2);
-//	//将函数的结果压入栈中。如果有多个返回值，可以在这里多次压入栈中。
-//	lua_pushnumber(L, op1 + op2);
-//	//返回值用于提示该C函数的返回值数量，即压入栈中的返回值数量。
-//	return 1;
-//}
-//
-//lua_register(L,"name",add2)
